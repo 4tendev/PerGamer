951be7e4ab2c23803ddf81cb70704be087eb9561
4tendev <4tenprog@gmail.com>
2024-06-19 15:45:38 +0330

    response message finder

---
+++ front-end/commonTsBrowser/responseMessageFinder.ts
@@ -0,0 +1,20 @@
+import { Language } from "@/GlobalStates/Slices/languageSlice";
+import dictionary from "./dictionary.json";
+export default function responseMessage(
+  selectedDictionary: { [key: string]: { [key: string]: string } },
+  lang: Language["lang"],
+  code: string
+): string {
+  switch (code) {
+    case "500":
+      return dictionary["500"][lang];
+    case "429":
+      return dictionary["429"][lang];
+  }
+  try {
+    const response = selectedDictionary[code][lang];
+    return response;
+  } catch (error) {
+    return dictionary.unknownCode[lang];
+  }
+}
