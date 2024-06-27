cb9e32a092e52a00d7e35d6219952daf26af0b7a
4tendev <4tenprog@gmail.com>
2024-06-27 11:52:20 +0330

    saathoshie creatopr

--- back-end/private/market/signals.py
+++ back-end/private/market/signals.py
@@ -6,10 +6,10 @@ from .models import Asset
 @receiver(post_migrate)
 def create_default_instances(sender, **kwargs):
     if sender.name == 'market':
-        default_instances = [
-            {'name': 'BTC'},
-            {'name': 'USDT'},
-            {'name': 'TOMAN'},
-        ]
-        for instance_data in default_instances:
-            Asset.objects.get_or_create(**instance_data)
+            default_instances = [
+                {'name': 'SATOSHI' , "decimal" : 0},
+                {'name': 'USDT' ,"decimal" : 2},
+                {'name': 'TOMAN' ,"decimal" : 0},
+            ]
+            for instance_data in default_instances:
+                Asset.objects.get_or_create(**instance_data)
