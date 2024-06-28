8d97d0e263ee5f0343da0d6fc7944ee7e98b37b3
4tendev <4tenprog@gmail.com>
2024-06-28 23:37:53 +0330

    fix it wrong props

--- front-end/app/market/[game]/page.tsx
+++ front-end/app/market/[game]/page.tsx
@@ -2,10 +2,12 @@ import getData from "@/commonTsServer/getData";
 import Market from "../Market";
 
 export default async function Page({ params }: { params: { game: string } }) {
-  const data = await getData("/market/platforms/");
-  const platforms: Platform[] = data.data;
+  const platformsData = await getData("/market/platforms/");
+  const platforms: Platform[] = platformsData.data;
   const platform = platforms.find(
     (platform: Platform) => platform.game == params.game.toUpperCase()
   );
-  return <Market platforms={platforms} products={[]} platform={platform} />;
+  const marketData = await getData("/market/");
+  const market = marketData.data
+  return <Market platforms={platforms} market={market} platform={platform} />;
 }
