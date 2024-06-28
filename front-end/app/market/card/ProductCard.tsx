3ca16e0cc30a4ccf9d14aeeeb2b4f1f96a7f8017
4tendev <4tenprog@gmail.com>
2024-06-28 23:38:06 +0330

    smaller products

--- front-end/app/market/card/ProductCard.tsx
+++ front-end/app/market/card/ProductCard.tsx
@@ -22,87 +22,61 @@ const ProductCard = (props: { detail: Detail; products: Product[] }) => {
   );
 
   return (
-    <>
-      <div className="w-52  p-4  h-fit rounded-md flex flex-col gap-2 bg-base-200">
-        <div className="flex justify-between w-full text-[10px]">
+    <div className="w-36   h-fit rounded-md flex flex-col gap-1 bg-base-300 p-1 ">
+      <div className="w-full h-[110px]  flex flex-col items-start justify-start  relative">
+        <div className="flex justify-between w-full text-[10px] px-1 ">
           {selectedProduct.deliveryMethod == 1 ? (
             <>
-              <div className="">
-                { "deliver at less than day"}
-              </div>
+              <div className="">{"deliver  day"}</div>
               <div>2 H </div>
             </>
           ) : (
             <>
-              <div>
-                Dota2 Gift
-              </div>
-              <div>{"seller's steam profile"}</div>
+              <div>Dota2 Gift</div>
+              <div>{"seller's profile"}</div>
             </>
           )}
         </div>
-        <div className="w-full h-[160px]  flex flex-col items-start justify-start py-2 gap-1 z-0">
-          <img
-            width={170}
-            height={170}
-            className="mx-auto rounded-lg shadow-xl"
-            alt="Drow"
-            src={detail.img}
-          />
+        <img
+          width={170}
+          height={170}
+          className="mx-auto  shadow-xl rounded-md "
+          alt={detail.title}
+          src={detail.img}
+        />
+
+        <div className="absolute top-4 left-0">
           <Descriptions descriptions={selectedProduct.descriptions} />
         </div>
-        <div dir="ltr" className=" leading-3 my-1 ">
-          {selectedProduct.amount} {selectedProduct.asset}
+      </div>
+      <div
+        dir="ltr"
+        className=" leading-3  text-xs flex justify-between items-center pe-1"
+      >
+        {selectedProduct.amount}123222 {selectedProduct.asset}
+        <button className="  h-4 w-8 rounded-xl bg-success text-black">
+          +
+        </button>
+      </div>
+      <div>
+        <div
+          dir="ltr"
+          className="text-[11px] font-bold  h-4 w-full overflow-hidden"
+        >
+          {detail.title}
         </div>
-        <div>
-          <div
-            dir="ltr"
-            className="text-xs font-bold  h-4 w-full overflow-hidden"
-          >
-            {detail.title}
-          </div>
-          <div
-            dir="ltr"
-            className="flex text-[10px] gap-2 overflow-hidden font-mono"
-          >
-            {detail.tags.map((tag) => (
-              <div key={tag} className="broder rounded-lg text-info">
-                {tag}
-              </div>
-            ))}
-          </div>
-        </div>
-        <div className="flex justify-between text-xs items-center ">
-          <div>Sellers :</div>
-          {uniqueProducts.length > 0 && (
-            <select
-              value={selectedProduct.id}
-              onChange={(event) =>
-                setSelectedProduct(
-                  uniqueProducts?.find(
-                    (product) => product.id == Number(event.target.value)
-                  ) ?? uniqueProducts[0]
-                )
-              }
-              dir="ltr"
-              className="select select-xs "
-            >
-              {uniqueProducts.map((product) => (
-                <option key={product.id} value={product.id}>
-                  {product.id}
-                </option>
-              ))}
-            </select>
-          )}
-        </div>
-        <div className="flex justify-between">
-          <button className="btn btn-success btn-xs rounded-md">
-            add to card
-          </button>
-          <button className="btn btn-accent btn-xs rounded-md">See all</button>
+        <div
+          dir="ltr"
+          className="flex text-[10px] gap-2 overflow-hidden font-mono px-1"
+        >
+          {detail.tags.map((tag) => (
+            <div key={tag} className="broder rounded-lg text-info">
+              {tag}
+            </div>
+          ))}
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
