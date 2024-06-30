37372660f388507546fd8f9a9220712427bf6521
4tendev <4tenprog@gmail.com>
2024-06-30 13:22:29 +0330

    remove in card items from prodcuts

--- front-end/app/market/page.tsx
+++ front-end/app/market/page.tsx
@@ -1,15 +1,22 @@
 import getData from "@/commonTsServer/getData";
 import React from "react";
-import Market from "./Market"; 
+import Market from "./Market";
+import { cookies } from "next/headers";
 
 const Page = async () => {
   const platformsData = await getData("/market/platforms/");
-  const platforms :Platform[] = platformsData.data;
+  const platforms: Platform[] = platformsData.data;
   const marketData = await getData("/market/");
-  const market = marketData.data
-  return (
-    <Market  platforms={platforms} market={market} platform={undefined} />
+  const market = marketData.data as Market;
+  const cookieStore = cookies();
+  const card = cookieStore.get("ShoppingCard");
+
+  const shoppingCard = card?.value ? JSON.parse(card.value) : [];
+
+  market.products = market.products.filter(
+    (product) => !shoppingCard.includes(product.id)
   );
+  return <Market platforms={platforms} market={market} platform={undefined} />;
 };
 
 export default Page;
