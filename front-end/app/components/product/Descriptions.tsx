d9172c5c92457dfebb8ce4f9e80f2f8c8e00c04d
4tendev <4tenprog@gmail.com>
2024-06-30 13:21:18 +0330

    smarller gems

--- front-end/app/components/product/Descriptions.tsx
+++ front-end/app/components/product/Descriptions.tsx
@@ -10,7 +10,7 @@ const Descriptions = (props: { descriptions: Description[] }) => {
   }
 
   return (
-    <div className="flex gap-1 justify-center items-center shadow-2xl ">
+    <div className="flex gap-0.5 justify-center items-center flex-col">
       {descriptions.map((item, index) => {
         if (index < descriptions.length - 1) {
           const regex = /url\([^\)]+\)/g;
@@ -24,17 +24,17 @@ const Descriptions = (props: { descriptions: Description[] }) => {
               textOnly.length > 5 && (
                 <div
                   key={index + 1}
-                  className="tooltip "
+                  className="tooltip tooltip-right"
                   style={{ fontSize: "5px" }}
-                  data-tip={textOnly}
+                  data-tip={textOnly.substring(1,26)}
                 >
                   <img
                     key={indexed}
-                    width={25}
-                    height={25}
+                    width={23}
+                    height={23}
                     style={{ fontSize: "5px" }}
-                    className="mx-auto rounded-lg  tooltip "
-                    data-tip={textOnly}
+                    className="mx-auto rounded-lg  tooltip"
+                    data-tip={textOnly.substring(1,26)}
                     alt={""}
                     src={match.substring(4, match.length - 1)}
                   />
