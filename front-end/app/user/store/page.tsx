306b632d82b79452c0ddfc7495f422fd17e7895c
4tendev <4tenprog@gmail.com>
2024-07-01 22:22:41 +0330

    add new page for orders and products

--- front-end/app/user/store/page.tsx
+++ front-end/app/user/store/page.tsx
@@ -4,15 +4,30 @@ import React from "react";
 
 const Page = async () => {
   return (
-    <div className="w-fit mx-auto flex justify-center items-center gap-3 flex-col my-3">
-      <Link className="btn  w-full btn-primary btn-xs" href="">
+    <div className="mx-auto flex justify-center items-center gap-2 flex-col my-3 w-full max-w-md px-3">
+      <Link
+        className="btn  w-full btn-primary btn-sm  "
+        href="/user/store/orders"
+      >
         Open Orders
       </Link>
-      <Link className="btn w-full btn-primary btn-xs" href="/user/store/addproduct">
-        Add New Product
+      <Link
+        className="btn w-full btn-primary btn-sm  "
+        href="/user/store/orders"
+      >
+        Order History
       </Link>
-      <Link className="btn   w-full btn-primary btn-xs" href="">
-        manage Existing Products
+      <Link
+        className="btn  w-full  btn-primary btn-sm  "
+        href="/user/store/products"
+      >
+        Manage Products
+      </Link>
+      <Link
+        className="btn w-full  btn-primary btn-sm "
+        href="/user/store/addproduct"
+      >
+        Add New Product
       </Link>
     </div>
   );
